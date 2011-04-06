389d9998b66624b209fb15beb1589fc5bd939bc6
owlsroost <tony@owlsroost.org.uk>
2011-04-06 09:06:23 +0000

    v0076_part_DWM release

--- dshowhelper_DWM_mod/source/EVRCustomPresenter.cpp
+++ dshowhelper_DWM_mod/source/EVRCustomPresenter.cpp
@@ -3568,53 +3568,6 @@ void MPEVRCustomPresenter::GetRealRefreshRate()
   m_dD3DRefreshCycle = 1000.0 / m_dD3DRefreshRate; // in ms
 }
 
-//// get time delay (in hns) to target raster paint position
-//// returns zero delay if 'now' is inside the limitLow/limitHigh window
-//LONGLONG MPEVRCustomPresenter::GetDelayToRasterTarget(LONGLONG *offsetTime)
-//{
-//    D3DRASTER_STATUS rasterStatus;
-//    LONGLONG targetDelay = 0;
-//
-//    // Calculate raster offset
-//    if (SUCCEEDED(m_pD3DDev->GetRasterStatus(0, &rasterStatus)))
-//    {
-//      int currScanline = (int)rasterStatus.ScanLine;
-//      
-//      if ( currScanline < 2 )
-//      {
-//        //currScanline value is reported as zero all through vertical blanking
-//        //so limit delay to avoid overshooting the target position
-//        targetDelay = MIN_VSC_DELAY ; //Limit to 1.2 ms
-//        return targetDelay;
-//      }
-//      else if ( currScanline < m_rasterLimitLow )
-//      {
-//        targetDelay = (LONGLONG)(m_rasterTargetPosn - currScanline) * m_hnsScanlineTime ;       
-//      }
-//      else if ( currScanline > (m_rasterLimitHigh + (*offsetTime/m_hnsScanlineTime)) )
-//      {
-//        targetDelay = MIN_VSC_DELAY ; //Limit to 1.2 ms ;  
-//      }   
-//      
-//      //sanity check the delay value
-//      if (targetDelay < 0) 
-//      {
-//        targetDelay = MIN_VSC_DELAY;
-//      }
-//      else if (targetDelay > ((LONGLONG)m_rasterTargetPosn * m_hnsScanlineTime))
-//      {
-//        targetDelay = (LONGLONG)m_rasterTargetPosn * m_hnsScanlineTime;
-//      }
-//      
-//      *offsetTime = 0;
-//      if ((currScanline < (int)m_maxScanLine) && (targetDelay < MIN_VSC_DELAY))
-//      {
-//        *offsetTime = (LONGLONG)((int)m_maxScanLine - currScanline) * m_hnsScanlineTime ;
-//      }    
-//    }
-//    
-//    return targetDelay;
-//}
 
 
 // returns true if 'now' is inside the limitLow/limitHigh window
