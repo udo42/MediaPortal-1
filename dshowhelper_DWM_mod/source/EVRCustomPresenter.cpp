14c9a0391b4c79c6d900db820ff34a6386fc058c
owlsroost <tony@owlsroost.org.uk>
2011-08-07 20:23:38 +0000

    v0088a release (as v0088 but with queued mode DWM disabled)

--- dshowhelper_DWM_mod/source/EVRCustomPresenter.cpp
+++ dshowhelper_DWM_mod/source/EVRCustomPresenter.cpp
@@ -86,11 +86,11 @@ MPEVRCustomPresenter::MPEVRCustomPresenter(IVMR9Callback* pCallback, IDirect3DDe
     LogRotate();
     if (NO_MP_AUD_REND)
     {
-      Log("---------- v1.4.088 part DWM ----------- instance 0x%x", this);
+      Log("---------- v1.4.088a no DWM ----------- instance 0x%x", this);
     }
     else
     {
-      Log("---------- v0.0.088 part DWM ----------- instance 0x%x", this);
+      Log("---------- v0.0.088a no DWM ----------- instance 0x%x", this);
       Log("------- audio renderer testing --------- instance 0x%x", this);
     }
     m_hMonitor = monitor;
