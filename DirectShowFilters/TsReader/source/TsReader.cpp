220b714aff4529bb2aa077522251f5bd88b13af4
owlsroost <tony@owlsroost.org.uk>
2012-04-13 21:03:50 +0100

    v52 release

--- DirectShowFilters/TsReader/source/TsReader.cpp
+++ DirectShowFilters/TsReader/source/TsReader.cpp
@@ -181,7 +181,7 @@ CTsReaderFilter::CTsReaderFilter(IUnknown *pUnk, HRESULT *phr):
   GetLogFile(filename);
   ::DeleteFile(filename);
   LogDebug("----- Experimental noStopMod version -----");
-  LogDebug("---------- v0.0.52a XXX -------------------");
+  LogDebug("---------- v0.0.52 XXX -------------------");
   
   m_fileReader=NULL;
   m_fileDuration=NULL;
